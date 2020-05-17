cf1f9d35bc4691fc4043a5c1ef492bd85bdb1000
Osvaldo Contreras <ocontreras.tx@gmail.com>
2020-05-17 16:25:14 -0500

    Updated header padding

--- frontend/src/components/Followers.vue
+++ frontend/src/components/Followers.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="header">
+  <div class="header top__background">
     <div class="header__title">Social Media Followers</div>
     <div class="header__total__followers">Total Followers: 23,004</div>
     <div class="followers">
@@ -22,7 +22,21 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+$light-background-top-pale-blue: hsl(225, 100%, 98%);
+
+.top__background {
+  position: absolute;
+  background-color: $light-background-top-pale-blue;
+  width: 100%;
+  height: 244px;
+  border-radius: 0 0 25px 25px;
+}
+
+.header {
+  padding: 0 165px 0;
+}
+
 .header__title {
   font-size: 26px;
   font-weight: 700;
